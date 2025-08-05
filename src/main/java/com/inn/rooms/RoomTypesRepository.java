8a55f62e5ec5cc343382bb74dff8e12d61426726
FELT0416 <serenactb@gmail.com>
2025-08-05 17:42:41 +0900

    예약 상세 로직 구현

--- src/main/java/com/inn/rooms/RoomTypesRepository.java
+++ src/main/java/com/inn/rooms/RoomTypesRepository.java
@@ -5,10 +5,36 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface RoomTypesRepository extends JpaRepository<RoomTypes, Long> {
-    @Query("SELECT rt FROM RoomTypes rt WHERE rt.hotel_id = :hotelId")
+    @Query("SELECT rt FROM RoomTypes rt WHERE rt.hotelId = :hotelId")
     List<RoomTypes> findByHotelId(@Param("hotelId") Long hotelId);
+
+    @Query("""
+    SELECT DISTINCT rt
+    FROM RoomTypes rt
+    WHERE rt.capacity >= :peopleCount
+    AND EXISTS (
+        SELECT 1
+        FROM Rooms r
+        WHERE r.roomType = rt
+        AND r.hotelId = :hotelId
+        AND NOT EXISTS (
+            SELECT 1
+            FROM Reserve res
+            WHERE res.room = r
+            AND res.checkIn < :checkOutDate
+            AND res.checkOut > :checkInDate
+        )
+    )
+""")
+    List<RoomTypes> findAvailableRoomType(@Param("checkInDate") LocalDate checkInDate,
+                                          @Param("checkOutDate") LocalDate checkOutDate,
+                                          @Param("peopleCount") int peopleCount,
+                                          @Param("hotelId") Long hotelId);
+
+
 }
